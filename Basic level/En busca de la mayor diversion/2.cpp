8e5bc6397442e8b0baed776152177bd916e0f7b5
IYahir <ivanyahirmopi@gmail.com>
2022-10-14 00:30:33 -0500

    En busca de la mayor diversion. Con arreglo dinamico, 82%

--- Basic level/En busca de la mayor diversion/2.cpp
+++ Basic level/En busca de la mayor diversion/2.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-    int noJuguetes;
-    vector<unsigned short int> diversion;
-
+    //Se crea una variable para la cantidad de juguetes inicial.
+    unsigned int noJuguetes = 0;
     cin >> noJuguetes;
-    for (int i = 0; i < noJuguetes; i++){
-        int tmp;
-        cin >> tmp;
-        diversion.push_back(tmp);
-    }
+    //Se crea un arreglo dinamico de enteros con capacidad para los juguetes.
+    unsigned short int* diversion = new unsigned short int[noJuguetes];
 
-    sort(diversion.begin(), diversion.end(), greater<int>());
-    noJuguetes = 0;
-    for (int i = 0; i < diversion.size() - 1; i++)
-        noJuguetes += diversion[i];
-    cout << noJuguetes;
+    //Se le asigna una entrada a cada punto del arreglo.
+    for (unsigned int i = 0; i < noJuguetes; i++)
+        cin >> *(diversion + i);
+
+    //Se ordena de mayor a menor el arreglo
+    sort(diversion, diversion + (noJuguetes), greater<unsigned short int>());
+
+    //Se crea una variable para el resultado
+    int resultado = 0;
+    //Se itera desde 0 hasta el penultimo juguete de la lista
+    //ordenada de mayor a menor por diversion del juguete.
+    //Y se suma al resultado.
+    for (unsigned int i = 0; i < noJuguetes - 1; i++)
+        resultado += diversion[i];
+    
+    //Se imprime el resultado.
+    cout << resultado;
+
+    //Se elimina el arreglo
+    delete[] diversion;
+    return 0;
 }
